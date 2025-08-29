63f82d00cbb0267cd0da5b4d7952e42ff3ed763c
Tay Wei Ting <weitingly123@gmail.com>
2025-08-29 13:03:14 +0800

    Update LoanProcessor.java

--- src/main/java/com/uob/LoanProcessor.java
+++ src/main/java/com/uob/LoanProcessor.java
@@ -1,8 +1,27 @@
 package com.uob;
 
+import java.util.List;
+
 public class LoanProcessor {
+
     public void processLoan() {
-        // TODO: add proper validation (Sonar will flag TODOs)
-        System.out.println("Processing loan.x.");
+        // Bug 1: Null pointer
+        String clientName = null;
+        System.out.println(clientName.length()); // NullPointerException
+
+        // Bug 2: Array index out of bounds
+        int[] loanAmounts = {1000, 2000, 3000};
+        System.out.println(loanAmounts[5]);
+
+        // Bug 3: Division by zero
+        int totalLoan = 10000;
+        int numberOfClients = 0;
+        int averageLoan = totalLoan / numberOfClients; // ArithmeticException
+        System.out.println("Average loan: " + averageLoan);
+
+        // Bug 4: Unchecked generic cast
+        Object obj = List.of("loan1", "loan2");
+        List<Integer> loans = (List<Integer>) obj; // ClassCastException at runtime
+        System.out.println(loans);
     }
 }
